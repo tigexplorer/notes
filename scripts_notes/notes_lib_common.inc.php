a74ca3e6d89274b97d257574b1936bc5052a8600
xpilgrim <joergsorge@gmail.com>
2015-01-08 18:02:15 +0100

    changed scripts-path

---
+++ scripts_notes/notes_lib_common.inc.php
@@ -0,0 +1,47 @@
+<?php
+
+/** 
+* library for common functions 
+*
+* PHP version 5
+*
+* @category Site
+* @package  Notes
+* @author   Joerg Sorge <joergsorge@googel.com>
+* @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+* @link     http://joergsorge.de
+*/
+
+function search_forbidden_strings( $my_string ) {
+    $string_ok = "yes"; 
+    if(strpos($my_string,"Select")!==false){ $string_ok = "no";}
+    if(strpos($my_string,"Update")!==false){ $string_ok = "no";}
+    if(strpos($my_string,"Union")!==false){ $string_ok = "no";}
+    if(strpos($my_string,"exec")!==false){ $string_ok = "no";}
+	return $string_ok;	
+}
+	
+function html_menu( $current_item ) {
+    echo "<ul class='nav nav-pills'>\n";
+    echo "<li><a href='index.php'>Home</a></li>\n";
+    if ( $current_item == "main") {
+        echo "<li class='active'><a href='notes_main_list.php?action=list&find_option=p_and_c'>Main</a></li>\n";
+    } else {
+        echo "<li><a href='notes_main_list.php?action=list&find_option=p_and_c'>Main</a></li>\n";
+    }
+    if ( $current_item == "all" ) {		
+	       echo "<li class='active'><a href='notes_find_list.php?action=list&find_option=all'>All</a></li>\n";
+    } else {
+        echo "<li><a href='notes_find_list.php?action=list&find_option=all'>All</a></li>\n";
+    }
+    if ( $current_item == "find" ) {		
+	       echo "<li class='active'><a href='notes_find.php'>Find</a></li>\n";
+    } else {
+        echo "<li><a href='notes_find.php'>Find</a></li>\n";
+    }
+    echo "<li><a href='notes_edit.php?action=new&amp;pa=0'>New Main</a></li>\n";
+    echo "</ul>\n";
+}
+
+	
+?>
---
+++ scripts_notes/notes_lib_db.inc.php
@@ -0,0 +1,59 @@
+<?php
+
+/** 
+* library for db functions 
+*
+* PHP version 5
+*
+* @category Site
+* @package  Notes
+* @author   Joerg Sorge <joergsorge@googel.com>
+* @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+* @link     http://joergsorge.de
+*/
+
+require "notes_db_conf.inc.php";
+
+function db_query_list_items_1( $table, $fields, $condition ){
+    $db = db_connect_pdo();
+    if ( substr($condition, 0, 7) == "NOWHERE" ) {
+        // condition enthaelt z.B. ORDER BY
+        $stmt = $db->query('SELECT '.$fields.' FROM '.$table.' '.substr($condition,7));
+    }else{
+        $stmt = $db->query('SELECT '.$fields.' FROM '.$table.' WHERE '.$condition);
+    }
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $result;
+}
+
+function db_query_display_item_1( $table, $condition ){
+    $db = db_connect_pdo();
+    $stmt = $db->query('SELECT * FROM '.$table.' WHERE '.$condition);
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    return $result;
+}
+
+function db_query_update_item_1( $table, $fields, $value1, $value2, $value3, $id ){
+    // item aktualisieren
+    $db = db_connect_pdo();
+    $stmt = $db->prepare("UPDATE ".$table." SET ".$fields." WHERE id=?");
+    $stmt->execute(array($value1, $value2, $value3, $id));
+
+}
+
+function db_query_add_item_1( $table, $fields, $value1, $value2, $value3, $value4 ) {
+    // row zufuegen
+    $db = db_connect_pdo();
+    $stmt = $db->prepare("INSERT INTO ".$table."(".$fields.") VALUES(:value1,:value2,:value3,:value4)");
+    $stmt->execute(array(':value1' => $value1, ':value2' => $value2, ':value3' => $value3, ':value4' => $value4));
+    $insertId = $db->lastInsertId();
+    return $insertId;
+}
+
+function db_query_delete_item_1( $table, $id ){
+    $db = db_connect_pdo();
+    $stmt = $db->prepare("DELETE FROM ".$table." WHERE id=:id");
+    $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+    $stmt->execute();
+}
+?>
